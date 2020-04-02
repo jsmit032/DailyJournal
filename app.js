2086bcd6a0e9e8ba6cce4f0c860ff5b83edc0973
Jennifer <jsmit032@ucr.edu>
2020-04-01 21:15:21 -0500

    add lodash package, impliment _.lowerCase to compare url and posts titles

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const lodash = require("lodash");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,7 +16,20 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let posts = [];
+let posts = [
+  {
+    title: 'Test',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vestibulum lorem sed risus ultricies tristique nulla. Tristique et egestas quis ipsum suspendisse. Nibh praesent tristique magna sit amet purus gravida quis blandit. Blandit massa enim nec dui nunc mattis. Fames ac turpis egestas sed tempus urna et pharetra pharetra. Non consectetur a erat nam at lectus. Dui accumsan sit amet nulla facilisi morbi tempus iaculis urna. Massa sapien faucibus et molestie ac feugiat sed lectus. Duis ut diam quam nulla porttitor massa. Mauris augue neque gravida in.'
+  },
+  {
+    title: 'Testing',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vestibulum lorem sed risus ultricies tristique nulla. Tristique et egestas quis ipsum suspendisse. Nibh praesent tristique magna sit amet purus gravida quis blandit. Blandit massa enim nec dui nunc mattis. Fames ac turpis egestas sed tempus urna et pharetra pharetra. Non consectetur a erat nam at lectus. Dui accumsan sit amet nulla facilisi morbi tempus iaculis urna. Massa sapien faucibus et molestie ac feugiat sed lectus. Duis ut diam quam nulla porttitor massa. Mauris augue neque gravida in.'
+  },
+  {
+    title: 'Another Post',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vestibulum lorem sed risus ultricies tristique nulla. Tristique et egestas quis ipsum suspendisse. Nibh praesent tristique magna sit amet purus gravida quis blandit. Blandit massa enim nec dui nunc mattis. Fames ac turpis egestas sed tempus urna et pharetra pharetra. Non consectetur a erat nam at lectus. Dui accumsan sit amet nulla facilisi morbi tempus iaculis urna. Massa sapien faucibus et molestie ac feugiat sed lectus. Duis ut diam quam nulla porttitor massa. Mauris augue neque gravida in.'
+  }
+];
 
 app.get('/', function(req, res){
   res.render("home", {homeConent: homeStartingContent, posts: posts});
@@ -45,10 +58,11 @@ app.post('/compose', function(req, res){
 });
 
 app.get('/posts/:postTitle', function(req, res) {
-  const requestedTitle = req.params.postTitle;
+  const requestedTitle = _.lowerCase(req.params.postTitle);
 
   for (var i=0; i<posts.length; i++) {
-      if (requestedTitle == posts[i].title) {
+    const storedTitle = _.lowerCase(posts[i].title);
+      if (requestedTitle == storedTitle) {
         console.log("It's a Match!");
         return;
       }
