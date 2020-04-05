c59ff5bdfbfa5c76bc7b501b8c64f4dee8136bac
Jennifer <jsmit032@ucr.edu>
2020-04-05 13:56:06 -0500

    changed titles to be Capitalized in DB

--- app.js
+++ app.js
@@ -26,17 +26,17 @@ const postSchema = new mongoose.Schema({
 const Post = mongoose.model("Post", postSchema);
 
 const homeStartingContent = new Post ({
-  title: "home",
+  title: "Home",
   content: "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing."
 });
 
 const aboutContent = new Post ({
-  title: "about",
+  title: "About",
   content: "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui."
 });
 
 const contactContent = new Post({
-  title: "contact",
+  title: "Content",
   content: "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero."
 });
 
@@ -57,8 +57,7 @@ app.get('/', function(req, res){
       res.redirect('/');
     } else {
       const newPosts = foundPosts.slice(3);
-      console.log(newPosts);
-      Post.findOne({ title: "home" }, function(err , foundPost){
+      Post.findOne({ title: "Home" }, function(err , foundPost){
         if (!err) {
           res.render('home', { homeContent: foundPost, posts: newPosts});
         }
@@ -68,7 +67,7 @@ app.get('/', function(req, res){
 });
 
 app.get('/about', function(req, res){
-  Post.findOne({ title: "about" }, function(err , foundPost){
+  Post.findOne({ title: "About" }, function(err , foundPost){
     if (!err) {
       console.log()
       res.render('about', { aboutContent: foundPost });
@@ -77,7 +76,7 @@ app.get('/about', function(req, res){
 });
 
 app.get('/contact', function(req, res){
-  Post.findOne({ title: "contact" }, function(err , foundPost){
+  Post.findOne({ title: "Contact" }, function(err , foundPost){
     if (!err) {
       console.log()
       res.render('contact', { contactContent: foundPost });
@@ -90,7 +89,7 @@ app.get('/compose', function(req, res){
 });
 
 app.post('/compose', function(req, res){
-  const postTitle = _.lowerCase(req.body.postTitle);
+  const postTitle = _.capitalize(req.body.postTitle);
   const postContent = req.body.postBody;
 
   var newPost = new Post({
@@ -101,7 +100,7 @@ app.post('/compose', function(req, res){
   Post.findOne( {title: postTitle}, function(err, foundPost){
     if (!err) {
       if (!foundPost) {
-        // create, push, save, and redirect to homepage
+        // save and redirect to homepage
         newPost.save(function (err) {
           if (!err) {
             res.redirect('/');
